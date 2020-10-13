5e394d2df9d0b933a75a5b9309634cdea4bd62cf
mblogger <mohitblogger@gmail.com>
2020-10-13 22:54:44 +0530

    BS-2 Inheritance and custom modifier

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Types = artifacts.require("Types");
+const Inheritance = artifacts.require("Inheritance");
 
 module.exports = function (deployer) {
     deployer.deploy(Types);
+    deployer.deploy(Inheritance);
 }
